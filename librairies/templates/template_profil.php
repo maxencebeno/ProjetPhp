87d83ed2f3e6252f220f325ae6a41dcbd4628a81
vincentmoral <vincent.moral69@yahoo.fr>
2014-05-13 08:45:30 +0200

    1.0.8

---
+++ librairies/templates/template_profil.php
@@ -0,0 +1,42 @@
+<?php
+	if(isset($_POST['old_password']) AND isset($_POST['new_password'])){
+		$_POST['old_password'] = strip_tags($_POST['old_password']);
+		$_POST['new_password'] = strip_tags($_POST['new_password']);
+			
+		if(!empty($_POST['new_password']) AND !empty($_POST['old_password'])) //check pseudo, nom et prénom pas vide
+		{
+			if (!strpos($_POST['new_password'], " ")){
+				if(strlen($_POST['new_password']) >= 8 AND strlen($_POST['new_password'] <= 40)) // check la longueur du password (entre 8 et 40 car.)
+				{
+					if(preg_match("#^(?=.*\d)(?=.*[a-z]).{8,20}$#", $_POST['new_password'])) //On teste si il y a bien des chiffres, et des minuscules dans le password
+					{
+						// On vérifie maintenant que l'ancien mot de passe du compte est correct pour valider le changement
+						$req = $bdd->prepare('SELECT * FROM user WHERE id_user = ? AND pass_user = ?');
+						$req->execute(array($_SESSION['id_user'], sha1($_POST['old_password'])));
+						$resultat = $req->fetch();
+						if($resultat){
+							// Requete de test
+								// Si ok
+								$req = $bdd->prepare('UPDATE user SET pass_user = :pass_user WHERE id_user = :id_user');
+								$req->execute(array(
+											'pass_user' => sha1($_POST['new_password']),
+											'id_user' => $_SESSION['id_user']
+								));
+								$succes_register = 'La modification a bien été prise en compte.';
+						}else{
+						$erreur_register = 'Erreur: ton ancien mot de passe n\'est pas celui-ci !';
+					}
+					}else{
+						$erreur_register = 'Erreur: ton mot de passe doit contenir au moins une lettre minuscule et un chiffre !';
+					}
+				}else{
+					$erreur_register = 'Erreur: ton mot de passe doit faire entre 8 et 40 caractères !';
+				}
+			}else{
+				$erreur_register = 'Erreur: ton mot de passe ne doit pas contenir d\'espaces !';
+			}
+		}else{
+			$erreur_register = 'Erreur: tu dois indiquer ton mot de passe !';
+		}
+	}		
+?> 
--- profil.php
+++ profil.php
@@ -1,11 +1,12 @@
 <?php 
 	session_start();
-	require_once('includes/db_connect.php');
-	$bdd = connect_db();	
-	require_once('includes/auto_login.php');
 	if(!isset($_SESSION['estConnecte'])){
 		header('Location: index.php');
 	}
+	require_once('includes/db_connect.php');
+	$bdd = connect_db();	
+	require_once('includes/auto_login.php');
+	require_once('librairies/templates/template_profil.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,6 +22,7 @@
 			<div id="profil_box">
 				<h1>Mon profil</h1>
 				<div id="main_box">
+					<?php if(!isset($_GET['update'])){ ?>
 					<div id="profil_pic"><img src="images/avatar.png" alt="profile_picture"/></div>
 					<div id="infos_box">
 						<p><span class="infos_box_label">Prénom:</span> <?php echo $_SESSION['prenom']; ?></p>
@@ -29,8 +31,27 @@
 						<p><span class="infos_box_label">E-mail:</span> <?php echo $_SESSION['mail']; ?></p>
 						<p><span class="infos_box_label">Date d'inscription:</span> <?php echo $_SESSION['date_inscription']; ?></p>
 					</div>
+					<?php
+					}else{
+					?>
+					<h3>Changement de mot de passe</h3>
+					<form method="POST" action="#" id="form_update">
+						<input type="password" name="old_password" placeholder="Ancien mot de passe" maxlength="20" id="old_password" required /><br />
+						<input type="password" name="new_password" placeholder="Nouveau mot de passe" maxlength="20" id="new_password" required /><br />
+						<input type="submit" value="Enregistrer" id="submit"/><br />
+						<?php if(isset($erreur_register)){echo '<p style="color: red;margin-top: 20px">'.$erreur_register.'</p>';} ?>
+						<?php if(isset($succes_register)){echo '<p style="color: green;margin-top: 20px">'.$succes_register.'</p>';} ?>
+					</form>
+					<?php } ?>
 				</div>
-				<a href="profil.php?c=update"><button id="update_button">Modifier ces informations</button></a>
+				<?php 
+				if(!isset($_GET['update'])){
+					echo '<a href="profil.php?update=1"><button id="update_button">Modifier ces informations</button></a>'; 
+					}else{
+						echo '<a href="profil.php"><button id="update_button">Annuler</button></a>';
+					}
+
+				?>
 			</div>
 		</div>
 
