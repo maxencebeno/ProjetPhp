8c5d7ad733d924cfc877b5d7ab2d39849a32e77c
maxencebeno <maxence.beno@gmail.com>
2014-05-13 08:34:41 +0200

    v1.0.6

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
 	-->
 	<nav id="menu">
 		<ul>
-			<li><a href="http://localhost/projetphp/index.php">Accueil</a></li>
+			<li><a href="index.php">Accueil</a></li>
 			<?php 
 			if(isset($_SESSION['estConnecte']) AND ($_SESSION['estConnecte'] == true)) {
 				echo '<li><a href="play.php">Révise tes maths !</a></li>';
@@ -14,7 +14,7 @@
 				echo '<li><a href="profil.php">Mon profil</a></li>';
 				echo '<li style="float: right"><a href="logout.php">Déconnexion</a></li>';
 			}else{
-				echo'<li><a href="http://localhost/projetphp/create-account.php">Inscription</a></li>';
+				echo'<li><a href="create-account.php">Inscription</a></li>';
 				?>
 				<form method="post" action="login.php" id="form_login">
 					<input type="text" name="pseudo" placeholder="Pseudo" maxlength="20" id="pseudo_login" required />
